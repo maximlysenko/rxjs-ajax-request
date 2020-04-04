0171315b70a2836183b27580bc14b7302a16415d
Max Lysenko <addition.mail.address@gmail.com>
2020-04-04 19:35:04 +0300

    Updated rollup build.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
 import babel from "rollup-plugin-babel"
 import commonjs from "rollup-plugin-commonjs"
+import {uglify} from "rollup-plugin-uglify"
 
 export default {
 	input: "src/createRequester.js",
 	output: {
 		file: "dist/index.js",
-		format: "es",
+		format: "cjs",
 	},
 	external: ["rxjs/ajax", "querystring"],
 	plugins: [
 		babel(),
 		commonjs(),
+		uglify(),
 	],
 }
